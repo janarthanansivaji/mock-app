8159816a26d2c552b18f8bd3ad8115347502aea3
jana <janacse06@gmail.com>
2021-11-22 18:48:58 +0530

    v2chnages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Yes Auto deploy working v1.1.1
+          Yes Auto deploy working v1.1.2
         </p>
         <a
           className="App-link"
