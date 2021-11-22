a011901cec2706c129b9dc6e25cb9da0d0d74faa
jana <janacse06@gmail.com>
2021-11-22 18:40:56 +0530

    sameple code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Yes Auto deploy working
+          Edit <code>src/App.js</code> and save to reload. Yes Auto deploy working v1.1.1
         </p>
         <a
           className="App-link"
