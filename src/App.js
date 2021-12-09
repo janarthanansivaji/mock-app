ca85de9e93a651ab6ca79ab173da0dbaf4bd017e
jana <janacse06@gmail.com>
2021-12-09 20:40:07 +0530

    chnages2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World new deploy
+          Hello World new deploy in Main
         </p>
         <a
           className="App-link"
