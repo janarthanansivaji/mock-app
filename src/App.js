e553fc82a1c5cf2edf0bea6c17048282d5c712b2
jana <janacse06@gmail.com>
2021-12-09 20:57:22 +0530

    chn23

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World new deploy np main.yml found in sunny
+          Hello World new deploy np main.yml added in sunny
         </p>
         <a
           className="App-link"
