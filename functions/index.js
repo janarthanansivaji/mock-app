537e230697581df9841ba4d29e8d97c54fb52ef0
jana <janacse06@gmail.com>
2021-12-15 18:58:29 +0530

    updated yml

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,6 @@ const functions = require("firebase-functions");
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase with actions support!");  
+  response.send("Hello from Firebase with actions support! new YML FILE");  
 });
 
