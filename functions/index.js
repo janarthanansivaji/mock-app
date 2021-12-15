1a5a3087d1a93eca274912cdae98c3687a8efdfc
jana <janacse06@gmail.com>
2021-12-15 13:30:54 +0530

    mock2

--- functions/index.js
+++ functions/index.js
@@ -6,5 +6,6 @@ const functions = require("firebase-functions");
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase with actions support!");
+  
 });
 
